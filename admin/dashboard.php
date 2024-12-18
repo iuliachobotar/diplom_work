5b9cb40415ef44f8b67e3efed0739ab503bd7a0e
iuliachobotar <chobotar_ly@ukr.net>
2024-12-18 17:14:26 +0200

    start back

---
+++ admin/dashboard.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard</title>
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css">
+
+    <link rel="stylesheet" href="../css/admin_style.css">
+</head>
+<body>
+    
+<?php include '../components/admin_header.php'; ?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<?php include '../components/footer.php'; ?>
+
+<script src="../js/admin_script.js"></script>
+
+
+</body>
+</html>
+
+
+
---
+++ components/admin_header.php
@@ -0,0 +1,8 @@
+<header class="header">
+
+<section class="flex">
+
+    <a href="dashboard.php" class="logo">Admin.</a>
+
+</section>
+</header>
