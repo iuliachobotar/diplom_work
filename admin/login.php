610d92c36a2e79a6a83a8d0dbf827643bb26643e
iuliachobotar <chobotar_ly@ukr.net>
2024-12-20 20:14:44 +0200

    fucking bug with tutor_id / finish register and login page

--- admin/login.php
+++ admin/login.php
@@ -9,64 +9,23 @@ if(isset($_COOKIE['tutor_id'])){
 
     if(isset($_POST['submit'])){
 
-        $id = create_unique_id();
-        $name = $_POST['name'];
-        $name = filter_var($name, FILTER_SANITIZE_STRING);
-
-        $profession = $_POST['profession'];
-        $profession = filter_var($profession, FILTER_SANITIZE_STRING);
-
         $email = $_POST['email'];
         $email = filter_var($email, FILTER_SANITIZE_STRING);
 
         $pass = sha1($_POST['pass']);
         $pass = filter_var($pass, FILTER_SANITIZE_STRING);
 
-        $c_pass = sha1($_POST['c_pass']);
-        $c_pass = filter_var($c_pass, FILTER_SANITIZE_STRING);
+        $verify_tutor = $conn->prepare("SELECT * FROM `tutor` WHERE email = ? AND password = ? LIMIT 1");
+        $verify_tutor->execute([$email, $pass]);
+        $row = $verify_tutor->fetch(PDO::FETCH_ASSOC);
 
-        $image = $_FILES['image']['name'];
-        $image = filter_var($image, FILTER_SANITIZE_STRING);
-        $ext = pathinfo($image, PATHINFO_EXTENSION);
-        $rename = create_unique_id().'.'.$ext;
-        $image_tmp_nname = $_FILES['image']['tmp_name'];
-        $image_size = $_FILES['image']['size'];
-        $image_folder = '../uploaded_files/'.$rename;
-
-        $select_tutor_email = $conn->prepare("SELECT * FROM `tutor` WHERE email = ?");
-        $select_tutor_email->execute([$email]);
-
-        if($select_tutor_email->rowCount() > 0){
-            $message [] = 'email already taken!';
+        if($verify_tutor->rowCount() > 0){
+            setcookie('tutor_id', $row['id'], time() + 60*60*24*30, '/');
+            header('location:dashboard.php');
         }else{
-            if($pass != $c_pass){
-                $message[] = 'password not matched!';
-            }else{
-                
-                if($image_size > 2000000){
-                    $message[] = 'image size to large!';
-                }else{
-                    $insert_tutor = $conn->prepare("INSERT INTO `tutor` (id, name, profession, email, password, image)VALUES(?,?,?,?,?,?)");
-                    $insert_tutor->execute([$id, $name, $profession, $email, $c_pass, $rename]);
-                    move_uploaded_file($image_tmp_name, $image_folder);
-
-
-                    $verify_tutor = $conn->prepare("SELECT * FROM `tutor` WHERE email = ? AND password = ? LIMIT 1");
-                    $verify_tutor->execute([$email, $c_pass]);
-                    $row = $verify_tutor->fetch(PDO::FETCH_ASSOC);
-
-                    if($insert_tutor){
-                        if($verify_tutor->rowCount() > 0){
-                            setcookie('tutor_id', $row['id'], time() + 60*60*24*30, '/');
-                            header('location:dashboard.php');
-                        }else{
-                            $message[] = 'something went wrong!';
-                        }
-                    }
-                }
-
-            }
+            $message[] = 'incorrect email or password!';
         }
+                    
 
     }
 
@@ -110,7 +69,7 @@ if(isset($message)){
     <p>your password</p>
     <input type="password" name="pass" maxlength="20" required placeholder="enter your password" class="box">
 
-<input type="submit" value="register now" name="submit" class="btn" >
+<input type="submit" value="login now" name="submit" class="btn" >
 <p class="link">don't have an account? <a href="register.php">register new</a></p>
 
 </form>
