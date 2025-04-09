c1098a6b5890b9c0114d6ef9131045901cf6e512
iuliachobotar <chobotar_ly@ukr.net>
2025-04-09 10:49:09 +0300

    dark mode

--- js/admin_script.js
+++ js/admin_script.js
@@ -1,9 +1,10 @@
 document.addEventListener('DOMContentLoaded', () => {
-    let footer = document.querySelector('.footer');
     let body = document.body;
     let profile = document.querySelector('.header .flex .profile');
     let searchForm = document.querySelector('.header .flex .search-form');
     let sideBar = document.querySelector('.side-bar');
+    let toggleBtn = document.querySelector('#toggle-btn');
+    let darkMode = localStorage.getItem('dark-mode');
 
     let userBtn = document.querySelector('#user-btn');
     if (userBtn) {
@@ -46,4 +47,32 @@ document.addEventListener('DOMContentLoaded', () => {
             body.classList.remove('active');
         }
     };
+
+    // Перевірка темної теми при завантаженні
+    const enableDarkMode = () => {
+        toggleBtn.classList.replace('fa-sun', 'fa-moon');
+        body.classList.add('dark');
+        localStorage.setItem('dark-mode', 'enabled');
+    }
+
+    const disableDarkMode = () => {
+        toggleBtn.classList.replace('fa-moon', 'fa-sun');
+        body.classList.remove('dark');
+        localStorage.setItem('dark-mode', 'disabled');
+    }
+
+    // Використовуємо темну тему, якщо вона активована в localStorage
+    if (darkMode === 'enabled') {
+        enableDarkMode();
+    }
+
+    // Логіка перемикання темної теми
+    toggleBtn.onclick = () => {
+        let darkMode = localStorage.getItem('dark-mode');
+        if (darkMode === 'disabled') {
+            enableDarkMode();
+        } else {
+            disableDarkMode();
+        }
+    }
 });
